372ed797670ecc4efc15c7dca05189ac76b3adf7
victoriaebaronh <72566279+victoriaebaronh@users.noreply.github.com>
2022-11-17 21:08:13 -0600

    comentario

--- Practica08/Query.sql
+++ Practica08/Query.sql
@@ -1,5 +1,9 @@
+-- Consulta i
 SELECT * FROM empleado WHERE nombre LIKE 'C%';
+
+
 SELECT * FROM planta WHERE genero = 'Haworthia';
+
 SELECT * FROM cliente WHERE fecha_nacimiento BETWEEN '2002-01-01' AND  '2002-12-31'
 
 -- Consulta V
