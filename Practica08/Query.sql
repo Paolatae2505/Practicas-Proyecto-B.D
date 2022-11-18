f73ba5b64e8bb5eb11e91f64104e2464021b9598
Paola V.B <Paola_VB@ciencias.unam.mx>
2022-11-18 16:50:13 -0600

    Mi consulta

--- Practica08/Query.sql
+++ Practica08/Query.sql
@@ -8,7 +8,7 @@ SELECT * FROM planta WHERE genero = 'Haworthia';
 SELECT * FROM venta_linea WHERE EXTRACT(MONTH FROM fecha_pedido) = 5;
 
 -- Consulta iv
-SELECT * FROM cliente WHERE fecha_nacimiento BETWEEN '2002-01-01' AND  '2002-12-31'
+SELECT * FROM cliente WHERE fecha_nacimiento BETWEEN '2002-06-01' AND  '2002-06-30'
 
 -- Consulta v
 SELECT * FROM vivero
