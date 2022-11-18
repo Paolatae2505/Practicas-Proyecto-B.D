9c8cfa0e783f04e748c71fbd686a50dcf4bbc7e4
dpreciado3 <dpreciado3@ciencias.unam.mx>
2022-11-18 16:23:41 -0600

    Consulta III añadida

--- Practica08/Query.sql
+++ Practica08/Query.sql
@@ -5,7 +5,7 @@ SELECT * FROM empleado WHERE nombre LIKE 'C%';
 SELECT * FROM planta WHERE genero = 'Haworthia';
 
 -- Consulta iii
-
+SELECT * FROM venta_linea WHERE EXTRACT(MONTH FROM fecha_pedido) = 5;
 
 -- Consulta iv
 SELECT * FROM cliente WHERE fecha_nacimiento BETWEEN '2002-01-01' AND  '2002-12-31'
