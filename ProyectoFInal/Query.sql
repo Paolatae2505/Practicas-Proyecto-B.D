d53af4e6e6d0c40c8f80cbff15b6d180294cf794
Paola V.B <Paola_VB@ciencias.unam.mx>
2022-12-06 17:17:28 -0600

    Consultas 2

--- ProyectoFInal/Query.sql
+++ ProyectoFInal/Query.sql
@@ -1,10 +1,10 @@
 --- CONSULTAS ----
 -- Consulta 1 
-SELECT CURPAgente FROM AsistenciaCurso WHERE IdCurso IN (1,2,3) AND horasAsistidas < 15;
+SELECT CURPAgente FROM AsistenciaCurso WHERE IdCurso IN (1,2,3) AND horasAsistencia < 15
 UNION
-SELECT CURPAgente FROM AsistenciaCurso WHERE IdCurso = 4 AND horasAsistidas < 16;
+SELECT CURPAgente FROM AsistenciaCurso WHERE IdCurso = 4 AND horasAsistencia < 16
 UNION
-SELECT CURPAgente FROM AsistenciaCurso WHERE IdCurso = 5 AND horasAsistidas < 17;
+SELECT CURPAgente FROM AsistenciaCurso WHERE IdCurso = 5 AND horasAsistencia < 17;
 
 -- Consulta 2
 SELECT * FROM AgenteTele WHERE Horario = 'Vespertino';
