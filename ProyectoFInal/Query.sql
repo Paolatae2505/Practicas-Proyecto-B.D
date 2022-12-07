ef85f5827cc25f964ca5ee58b8405ef94b5ce088
Paola V.B <Paola_VB@ciencias.unam.mx>
2022-12-06 20:32:45 -0600

    Consultas

--- ProyectoFInal/Query.sql
+++ ProyectoFInal/Query.sql
@@ -1,28 +1,36 @@
 --- CONSULTAS ----
 
 -- Consulta 1 --
--- Obtiene a los agentes (con la cantidas de horas Asistidas)
+-- Obtiene a los agentes (con la cantidad de horas Asistidas)
 -- que no cumplieron con su cuota de Horas de entrenamiento
 -- CURSO 1,2,4 = 15 horas 
 -- CURSO 3 = 16 horas
 -- CURSO 4 = 17 horas
-SELECT CURPAgente, SUM(HorasAsistencia) HorasAsistidas
+SELECT * FROM
+(SELECT CURPAgente, IdCurso ,SUM(HorasAsistencia) HorasAsistencia
 FROM AsistenciaCurso
-WHERE IdCurso IN (1,2,4) AND horasAsistencia < 15
-GROUP BY CURPAgente
+WHERE IdCurso IN (1,2,4)
+GROUP BY CURPAgente,IdCurso) AS NoCumplen1
+WHERE HorasAsistencia < 15
 UNION 
-SELECT CURPAgente, SUM(HorasAsistencia) HorasAsistidas
+SELECT * FROM
+(SELECT CURPAgente, IdCurso, SUM(HorasAsistencia) HorasAsistencia
 FROM AsistenciaCurso
-WHERE IdCurso = 3 AND horasAsistencia < 16
-GROUP BY CURPAgente
+WHERE IdCurso = 3 
+GROUP BY CURPAgente,IdCurso) AS NoCumplen2
+WHERE HorasAsistencia < 16
 UNION 
-SELECT CURPAgente, SUM(HorasAsistencia) AS HorasAsistidas
+SELECT * FROM
+(SELECT CURPAgente,  IdCurso, SUM(HorasAsistencia) AS HorasAsistencia
 FROM AsistenciaCurso
-WHERE IdCurso = 5 AND horasAsistencia < 17
-GROUP BY CURPAgente;
+WHERE IdCurso = 5
+GROUP BY CURPAgente,IdCurso) AS NoCumplen3
+WHERE HorasAsistencia < 17;
+
 -- Consulta 2 --
 -- Obtiene a los agentes que asisten al curso
 -- en un horario vespertino
+
 SELECT * FROM AgenteTele WHERE Horario = 'Vespertino';
 -- Consulta 3 --
 -- Obtiene a los agentes que asistieron  el dia '2021-07-16' 
