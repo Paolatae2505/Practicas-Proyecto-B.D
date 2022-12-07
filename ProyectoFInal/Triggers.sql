53c1aab4f1555ff0dccfe4e72e1246847190dd61
victoriaebaronh <72566279+victoriaebaronh@users.noreply.github.com>
2022-12-06 21:07:07 -0600

    Trigger horario AgenteTele

--- ProyectoFInal/Triggers.sql
+++ ProyectoFInal/Triggers.sql
@@ -18,3 +18,28 @@ BEFORE INSERT OR UPDATE
 ON sala
 FOR EACH ROW
 EXECUTE PROCEDURE costosalas();
+
+
+
+-- Trigger que al insertar o actualizar la tabla AgenteTele, checa que el valor de horario esté bien escrito
+-- Debe ser 'Matutino' o 'Vespertino'
+CREATE OR REPLACE FUNCTION checar_turno() RETURNS TRIGGER
+AS $$
+	DECLARE
+	HorarioN VARCHAR(12);
+BEGIN
+	IF (TG_OP = 'INSERT' or TG_OP = 'UPDATE') THEN 
+	SELECT Horario INTO HorarioN FROM AgenteTele
+	WHERE CURPAgente = NEW.CURPAgente;
+	IF (HorarioN != 'Vespertino' AND HorarioN != 'Matutino') THEN
+	RAISE EXCEPTION '% debe estar escrito como: Matutino o Vespertino', HorarioN;
+	END IF;
+	END IF;
+	RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql;	
+
+CREATE TRIGGER matutino_vespertino 
+AFTER INSERT OR UPDATE ON AgenteTele
+FOR EACH ROW EXECUTE PROCEDURE checar_turno();
